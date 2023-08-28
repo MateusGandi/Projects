c382bfa475193f98cd6eb9298c8987535f8c1f0a
= <mateusgandi3@gmail.com>
2023-08-28 11:49:36 -0300

    new project

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        [HttpPost]
+        public void adcionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine("APsee");
+        }
+    }
+}
